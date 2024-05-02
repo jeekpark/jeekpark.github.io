5ace38abcc05b7da57b78f3cee90655beda4007f
jeekpark <jeekpark@student.42seoul.kr>
2024-05-02 19:37:25 +0900

    Update: 2024-05-02

--- fast_push.bat
+++ fast_push.bat
@@ -1,11 +1,12 @@
 @echo off
-SET DATE=%DATE:~10,4%-%DATE:~4,2%-%DATE:~7,2%
+for /f "tokens=2 delims==" %%I in ('wmic os get localdatetime /value') do set datetime=%%I
+set date=%datetime:~0,4%-%datetime:~4,2%-%datetime:~6,2%
 
 git add .
 git status
 
 set /p VAL="Commit and push the changes? [yes(y) / no]: "
 if "%VAL%"=="y" (
-	git commit -m "Update: %DATE%"
+	git commit -m "Update: %date%"
 	git push
 )
